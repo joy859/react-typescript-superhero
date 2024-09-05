5a9c1b32d72c86cac8e3297fefa7cca2cb68d99f
GreatnessNkoko <nkokomoukan@gmail.com>
2024-09-05 10:27:23 +0100

    implemented login functionality

---
+++ src/Redux/userSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+import { userType } from "../Types";
+
+export const defaultUser: userType = {
+  id: "",
+  username: "",
+  email: "",
+  isOnline: false,
+  img: "",
+  creationTime: "",
+  lastseen: "",
+  bio: "",
+};
+
+const initialState = {
+  // user:[],
+  currentUser: defaultUser,
+  // currentSelectedUser:null
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const user = action.payload;
+
+      // store user in local storage
+      localStorage.setItem("superhero_user", JSON.stringify(user));
+
+      // SET LOGED IN USER
+      state.currentUser = user;
+    },
+    setUsers: (state, action) => {},
+  },
+});
+
+export const { setUser, setUsers } = userSlice.actions;
+
+export default userSlice.reducer;
